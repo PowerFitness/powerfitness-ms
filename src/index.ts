5ac4bfde774bd1c6b18b1aaa7dcb3dfd0ff44a07
Fawzieh Assad <fawziehassad@gmail.com>
2021-02-25 21:33:35 -0600

    testing

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(
-    `Example app listenExample app listenExample app listenExample app listenExample app listenExample
-      app listenExample app listening at http://localhost:${port}`
-  );
-});  
+  console.log(`Example app listening at http://localhost:${port}`);
+});
