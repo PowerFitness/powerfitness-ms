42cd0902eca840ebd9e153a5397f3de9d4b4ec03
@cjscholl <catelynjscholl@gmail.com>
2021-03-17 17:57:40 -0500

    fix: log

--- src/api/PlanApi.ts
+++ src/api/PlanApi.ts
@@ -34,7 +34,7 @@ export class PlanApi {
 			const planId: number = (await new UpsertPlanGoalsCoordinator(dbProvider, plan).upsertData());
 			res.send(planId.toString());
 		} catch (e) {
-			console.log(e.message)
+			console.error(e.message)
 			res.status(500);
 			res.end();
 		}
